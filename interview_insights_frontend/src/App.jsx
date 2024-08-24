d05f3743806b314bb191a947cda9b49ad2cb2269
kiranbaburaj <kiranbaburaj.t@gmail.com>
2024-08-24 11:31:34 +0530

    build production routes

--- interview_insights_frontend/src/App.jsx
+++ interview_insights_frontend/src/App.jsx
@@ -31,12 +31,15 @@ const App = () => {
   return (
     <Suspense fallback={<MyComponent />}>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/verify-otp" element={<VerifyOTP />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/my-component" element={<MyComponent />} />
+        <Route path="/admin/login" element={<AdminLogin />} /> {/* Added Admin Login route */}
+
         {/* Protected Admin Routes */}
         <Route
           path="/admindashboard"
@@ -70,6 +73,7 @@ const App = () => {
             </ProtectedRoute>
           }
         />
+
         {/* Jobseeker Routes */}
         <Route
           path="/dashboard/jobseeker"
@@ -80,7 +84,7 @@ const App = () => {
           }
         />
         <Route
-          path="/jobseekerapplications"
+          path="/jobseeker/applications"
           element={
             <ProtectedRoute roleRequired="jobseeker">
               <JobseekerJobs />
@@ -88,13 +92,14 @@ const App = () => {
           }
         />
         <Route
-          path="/jobseekerprofile"
+          path="/jobseeker/profile"
           element={
             <ProtectedRoute roleRequired="jobseeker">
               <JobseekerProfile />
             </ProtectedRoute>
           }
         />
+
         {/* Employer Routes */}
         <Route
           path="/dashboard/employer"
@@ -105,21 +110,85 @@ const App = () => {
           }
         />
         <Route
-          path="/EmployerCompanyManagement"
+          path="/employer/company-management"
           element={
             <ProtectedRoute roleRequired="employer">
               <EmployerCompanyManagement />
             </ProtectedRoute>
           }
         />
+        <Route
+          path="/employer/jobs"
+          element={
+            <ProtectedRoute roleRequired="employer">
+              <EmployerJobs />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/employer/applicants"
+          element={
+            <ProtectedRoute roleRequired="employer">
+              <ApplicantsList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/employer/schedule-interview"
+          element={
+            <ProtectedRoute roleRequired="employer">
+              <InterviewScheduler />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/employer/interview-feedback"
+          element={
+            <ProtectedRoute roleRequired="employer">
+              <InterviewFeedbackForm />
+            </ProtectedRoute>
+          }
+        />
+
         {/* Chat Routes */}
-        <Route path="/chat" element={<ProtectedRoute roleRequired="jobseeker"><Chat /></ProtectedRoute>} />
-        <Route path="/chat/:roomId" element={<ProtectedRoute roleRequired="jobseeker"><ChatRoom /></ProtectedRoute>} />
-        <Route path="/employer/chat" element={<ProtectedRoute roleRequired="employer" checkCompanyDetails={true}><Chat /></ProtectedRoute>} />
-        <Route path="/employer/chat/:roomId" element={<ProtectedRoute roleRequired="employer" checkCompanyDetails={true}><ChatRoom /></ProtectedRoute>} />
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute roleRequired="jobseeker">
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/chat/:roomId"
+          element={
+            <ProtectedRoute roleRequired="jobseeker">
+              <ChatRoom />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/employer/chat"
+          element={
+            <ProtectedRoute roleRequired="employer" checkCompanyDetails={true}>
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/employer/chat/:roomId"
+          element={
+            <ProtectedRoute roleRequired="employer" checkCompanyDetails={true}>
+              <ChatRoom />
+            </ProtectedRoute>
+          }
+        />
+
         {/* Job Details Route */}
         <Route path="/job/:jobId" element={<JobDetails />} />
-        {/* Add other routes here */}
+
+        {/* Catch-all Route for 404s */}
+        <Route path="*" element={<MyComponent />} />
       </Routes>
     </Suspense>
   );
