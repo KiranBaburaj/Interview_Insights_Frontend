6b03111ca03235601790f0dffdcc03fb69926a62
kiranbaburaj <kiranbaburaj.t@gmail.com>
2025-01-27 15:24:40 +0530

    logo changenew+ chat ui+build

--- interview_insights_frontend/dist/assets/Close-DZdotKmn.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-GsLKidK4.js";import{e as r}from"./List-iwZVGEJV.js";const e=r(o.jsx("path",{d:"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}),"Close");export{e as C};
